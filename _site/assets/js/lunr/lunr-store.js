f55099508edd7150fdfce8414c81f5c112bbdedf
Sam Fritz <svefritz@gmail.com>
2022-01-17 19:38:58 -0500

    major update

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Sample Social Network Datasets for Teaching",
+        "excerpt":"Sample Social Network Datasets For Teaching With Gephi (and Other Tools Like It).   Network datasets include: characters in the Game of Thrones universe; modernist authors who published together in journals; characters in the Marvel Universe; seventeenth-century Quakers; books about U.S. politics that were co-purchased around the 2004 presidential election; and people in TrumpWorld. A sample Gephi project created with the Game of Thrones network data can be viewed and explored as an interactive web network here.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/network-datasets/",
+        "teaser":"http://localhost:4000/images/got-network.png"},{
+        "title": "Tweets of a Native Son",
+        "excerpt":"A website featuring data visualizations about James Baldwin’s 21st-century digital afterlives that can be explored by students, fans, and scholars: #BlackLivesMatter tweets, popular YouTube videos, library circulation data, and more.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/tweets-of-a-native-son/",
+        "teaser":"http://localhost:4000/images/baldwin-logo-black-background.png"},{
+        "title": "a blog post page",
+        "excerpt":"   Title       Some intro text   Bold Header   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/intro-ca-jupyter-book/",
+        "teaser":null}]
